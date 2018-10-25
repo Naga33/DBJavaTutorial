6777ef6a6da095464b66af21513acec369700475
Naga33 <courtenay.mueller@gmail.com>
2018-10-25 13:39:48 +0200

    Add redundant comment to test git

--- app/src/main/java/com/example/android/dbjavatutorial/MainActivity.java
+++ app/src/main/java/com/example/android/dbjavatutorial/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,6 +19,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //added this line to test with git
     private WordViewModel wordViewModel;
     public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
